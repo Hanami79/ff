ed6d23fbcf69c49c0a4e674be8d05cd70c91bd3c
Hanami79 <sunrisetime615@gmail.com>
2025-11-23 04:17:02 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+
+    function createHeart() {
+        const heart = document.createElement("div");
+        heart.classList.add("heart");
+        heart.innerHTML = "❤";
+        heart.style.left = Math.random() * 100 + "vw";
+        heart.style.animationDuration = (3 + Math.random() * 3) + "s";
+        document.body.appendChild(heart);
+        setTimeout(() => heart.remove(), 6000);
+    }
+    setInterval(createHeart, 500);
